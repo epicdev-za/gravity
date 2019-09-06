f5bc9b0a7f9cb09a2a1be578fdbc59d6cc3ea546
Kalden Swart <kalden@epicdev.co.za>
2019-09-06 13:23:46 +0200

    - Setup repository - Setup npm package - Started writing initial tests

---
+++ index.js
@@ -0,0 +1,38 @@
+let restify = require("restify");
+
+let server = null;
+
+module.exports.start = function(callback, port = 3001){
+    if(server === null){
+        initializeServer(callback, port);
+    }else{
+        throw new Error("Server already initialized");
+    }
+};
+
+module.exports.stop = function(callback){
+    if(server !== null){
+        server.close(callback);
+    }else{
+        throw new Error("Server not initialized");
+    }
+};
+
+function initializeServer(callback, port){
+    server = restify.createServer({
+        name: 'Gravity Server',
+        version: process.env.npm_package_version
+    });
+
+    server.use(restify.plugins.acceptParser(server.acceptable));
+    server.use(restify.plugins.queryParser());
+    server.use(restify.plugins.bodyParser());
+
+    //@todo: load endpoints
+
+    server.listen(port, () => {
+        if(typeof callback !== undefined){
+            callback(server.name, server.url);
+        }
+    });
+}
---
+++ test/Main.js
@@ -0,0 +1,38 @@
+const assert = require("assert");
+const clients = require("restify-clients");
+const Gravity = require("../index");
+
+let client = clients.createJsonClient({
+    url: 'http://localhost:3001',
+    version: process.env.npm_package_version
+});
+
+describe('Gravity', function(){
+    describe('Setup', function(){
+        it('should setup the gravity server', function(done){
+            Gravity.start((name, url) => {
+                assert.strictEqual(name, "Gravity Server");
+                assert.strictEqual(url, "http://[::]:3001");
+                done();
+            });
+        });
+    });
+
+    describe('Stop', function(){
+        it('should stop the gravity server', function(done){
+            Gravity.stop(() => {
+                done();
+            });
+        });
+    });
+});
+
+//
+// client.get('/echo/mark', (err, req, res, obj) => {
+//     assert.ifError(err);
+//     console.log("Server returned: %j", obj);
+// });
+//
+// Gravity.stop(() => {
+//     console.log("Server closed");
+// });
