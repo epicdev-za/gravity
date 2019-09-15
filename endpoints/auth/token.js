9453c97b195fe1369de63c5ccdefa8c7edbe6298
InteferenceObject <admin@ot-sdt-serv.com>
2019-09-15 11:15:25 +0200

    - Finished password grant_type

--- endpoints/auth/token.js
+++ endpoints/auth/token.js
@@ -2,6 +2,7 @@ const Gravity = require("../../index");
 const Plasma = require("plasma");
 const sanitizer = require("../../utils/sanitizer");
 const Application = require("../../entities/Application");
+const clients = require("restify-clients");
 
 module.exports = function(req, res, next){
     if(req.body === undefined) req.body = {};
@@ -103,32 +104,79 @@ const grant_type = {
             const client_id = body['client_id'];
             const client_secret = body['client_secret'];
 
-            //@todo: Insert DB Query, User Entity, JWT, etc.
-            if(client_id === "test" && client_secret === "test"){
-                if(username === "test" && password === "test"){
+            Plasma.getConnection.fetch(Application, "SELECT * FROM " + Application.getEntity() + " WHERE client_id = $1 AND client_secret = $2", [client_id, client_secret], (err, r) => {
+                if (err){
+                    res.status(500);
                     res.send({
-                        access_token: "test",
-                        token_type: "bearer",
-                        expires_in: 3600,
-                        refresh_token: "test"
+                        error: Gravity.logError(err),
+                        error_description: "An internal server error occurred. Engineers have been notified, please try again later."
                     });
                     next();
                 }else{
-                    res.status(401);
-                    res.send({
-                        error: "invalid_grant",
-                        error_description: "Invalid username and password combination"
-                    });
-                    next();
+                    if(r.length === 1){
+                        let application = r[0];
+
+                        try{
+                            const config = require("../../gravity.config");
+                            let project_key = sanitizer.cleanUUID(config.sanctum.project_key);
+                            let location = config.sanctum.location;
+
+                            let client = clients.createJsonClient({
+                                url: location
+                            });
+
+                            client.post('/sanctum/auth', {
+                                project: project_key,
+                                username: username,
+                                password: password
+                            }, function(err, creq, cres, obj){
+                                if(err){
+                                    if(cres.statusCode === 500){
+                                        res.status(500);
+                                        res.send({
+                                            error: Gravity.logError(err),
+                                            error_description: "An internal server error occurred. Engineers have been notified, please try again later."
+                                        });
+                                        next();
+                                    }else{
+                                        res.status(cres.statusCode);
+                                        res.send(err.body);
+                                        next();
+                                    }
+                                }else{
+                                    application.authenticate(req.connection.remoteAddress, obj, req.headers['user-agent'], (err, token) => {
+                                        if(err){
+                                            res.status(500);
+                                            res.send({
+                                                error: Gravity.logError(err),
+                                                error_description: "An internal server error occurred. Engineers have been notified, please try again later."
+                                            });
+                                            next();
+                                        }else{
+                                            res.send(token);
+                                            next();
+                                        }
+                                    });
+                                }
+                            });
+                        }catch (err) {
+                            res.status(500);
+                            res.send({
+                                error: Gravity.logError(err),
+                                error_description: "An internal server error occurred. Engineers have been notified, please try again later."
+                            });
+                            next();
+                        }
+                    }else{
+                        res.status(401);
+                        res.send({
+                            error: "invalid_client",
+                            error_description: "Invalid client id and secret combination"
+                        });
+                        next();
+                    }
                 }
-            }else{
-                res.status(401);
-                res.send({
-                    error: "invalid_client",
-                    error_description: "Invalid client id and secret combination"
-                });
-                next();
-            }
+            });
 
         }
     }
